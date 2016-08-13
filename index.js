39e1b5305641bee7ff5b0eb93fceead728ec00be
rajivayyangar <rajivayyangar@gmail.com>
2016-08-13 11:56:20 -0700

    another fix - i to j

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function sendTextMessage(sender, text) {
 
 function makeQuickReplies(textArray){
     let QRarray = []
-    for (let i=0, i < textArray.length; i++){
+    for (let j=0, j < textArray.length; j++){
         quickReply = {
             "content_type":"text",
             "title":textArray[i],
