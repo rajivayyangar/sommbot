8dbad66a995500d32c9b040192df00f0f8edcda6
rajivayyangar <rajivayyangar@gmail.com>
2016-10-06 21:06:32 -0700

    added Fieldbook endpoint

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ const request = require('request')
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
+// Get data from Fieldbook database
+var requestify = require('requestify');
+var bookId = '57f718dd56cec00300626d43';
+var baseUrl = 'https://api.fieldbook.com/v1/' + bookId;
+var options = {
+    headers: {accept: 'application/json'},
+
+    auth: {
+        username: process.env.FIELDBOOK_USER,
+        password: process.env.FIELDBOOK_KEY
+    }
+};
+
+var url = baseUrl + '/available_wines';
+var response = await requestify.get(url, options);
+var responseObject = JSON.parse(response.body);
+responseObject = JSON.parse(response.body)
+//responseObject[0].wine_name
+//responseObject[0].description
+//responseObject[0].url
+
+
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
@@ -43,7 +65,7 @@ app.post('/webhook/', function (req, res) {
             if (text === 'Red'| text === 'Rose'){sendTextMessage(sender,'Sorry, I only know white wines now.')}
             else if (text === 'White') {
                 //let attributes = {}
-                sendTernaryAttributeMessage(sender,'What\'s the color concentration?')
+                sendTernaryAttributeMessage(sender,'What\'s the color concentration?'+responseObject[0].description)
                 continue
             }
             else if (text === 'diminished' | text === 'moderate' | text === 'elevated'){
